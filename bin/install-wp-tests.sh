e3a9858a8008dc575eba9c334112c4842501effd
v-i-nay <vinay@softscrips.net>
2025-07-23 17:36:14 +0530

    change

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/env bash
 
-# Exit if any command fails
+# Based on https://github.com/WordPress/wordpress-develop/blob/trunk/tests/phpunit/includes/install.php
+
 set -e
 
-# Set defaults
-DB_NAME=${1:-wordpress_test}
-DB_USER=${2:-root}
-DB_PASS=${3:-root}
-DB_HOST=${4:-127.0.0.1}
-WP_VERSION=${5:-latest}
+DB_NAME=$1
+DB_USER=$2
+DB_PASS=$3
+DB_HOST=${4-localhost}
+WP_VERSION=${5-latest}
 
-# Directory to install test suite and WordPress core
-WP_TESTS_DIR=${WP_TESTS_DIR:-/tmp/wordpress-tests-lib}
-WP_CORE_DIR=${WP_CORE_DIR:-/tmp/wordpress/}
+WP_TESTS_DIR="${WP_TESTS_DIR-/tmp/wordpress-tests-lib}"
+WP_CORE_DIR="${WP_CORE_DIR-/tmp/wordpress/}"
 
 download() {
   if [ "$(command -v curl)" ]; then
-    curl -sL "$1" > "$2"
+    curl -s "$1" -o "$2"
   elif [ "$(command -v wget)" ]; then
     wget -nv -O "$2" "$1"
   fi
@@ -25,27 +24,16 @@ download() {
 install_wp() {
   if [ ! -d "$WP_CORE_DIR" ]; then
     mkdir -p "$WP_CORE_DIR"
+    svn export --quiet https://core.svn.wordpress.org/tags/$WP_VERSION/ "$WP_CORE_DIR"
   fi
-
-  if [ "$WP_VERSION" == 'latest' ]; then
-    ARCHIVE_NAME='latest'
-  else
-    ARCHIVE_NAME="wordpress-${WP_VERSION}"
-  fi
-
-  download "https://wordpress.org/${ARCHIVE_NAME}.tar.gz" /tmp/wordpress.tar.gz
-  tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C "$WP_CORE_DIR"
 }
 
 install_test_suite() {
-  if [ ! -d "$WP_TESTS_DIR" ]; then
-    mkdir -p "$WP_TESTS_DIR"
-  fi
+  mkdir -p "$WP_TESTS_DIR"
 
-  svn export --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/includes/ "$WP_TESTS_DIR/includes"
-  svn export --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/data/ "$WP_TESTS_DIR/data"
-
-  download https://develop.svn.wordpress.org/tags/${WP_VERSION}/wp-tests-config-sample.php "$WP_TESTS_DIR/wp-tests-config.php"
+  svn export --quiet https://develop.svn.wordpress.org/tags/$WP_VERSION/tests/phpunit/includes/ "$WP_TESTS_DIR/includes"
+  svn export --quiet https://develop.svn.wordpress.org/tags/$WP_VERSION/tests/phpunit/data/ "$WP_TESTS_DIR/data"
+  svn export --quiet https://develop.svn.wordpress.org/tags/$WP_VERSION/wp-tests-config-sample.php "$WP_TESTS_DIR/wp-tests-config.php"
 
   sed -i "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" "$WP_TESTS_DIR/wp-tests-config.php"
   sed -i "s/yourdbnamehere/$DB_NAME/" "$WP_TESTS_DIR/wp-tests-config.php"
@@ -54,10 +42,10 @@ install_test_suite() {
   sed -i "s|localhost|$DB_HOST|" "$WP_TESTS_DIR/wp-tests-config.php"
 }
 
-setup_db() {
-  mysqladmin create "$DB_NAME" --user="$DB_USER" --password="$DB_PASS" --host="$DB_HOST" || true
+create_db() {
+  mysqladmin create "$DB_NAME" --user="$DB_USER" --password="$DB_PASS" -h "$DB_HOST" || true
 }
 
 install_wp
 install_test_suite
-setup_db
+create_db
