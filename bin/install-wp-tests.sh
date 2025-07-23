24a09692bfa4537b1f1f5293651956aafa257ca5
v-i-nay <vinay@softscrips.net>
2025-07-23 17:57:26 +0530

    change

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -5,33 +5,45 @@ set -e
 DB_NAME=$1
 DB_USER=$2
 DB_PASS=$3
-DB_HOST=$4
-WP_VERSION=${5:-latest}
-WP_TESTS_DIR=/tmp/wordpress-tests-lib
-WP_CORE_DIR=/tmp/wordpress
+DB_HOST=${4-localhost}
+WP_VERSION=${5-latest}
 
-# Install SVN if missing
-if ! command -v svn >/dev/null 2>&1; then
-  echo "SVN not found. Please install it."
-  exit 1
-fi
+WP_TESTS_DIR="${WP_TESTS_DIR-/tmp/wordpress-tests-lib}"
+WP_CORE_DIR="${WP_CORE_DIR-/tmp/wordpress/}"
 
-# Download test library
-mkdir -p "$WP_TESTS_DIR"
-svn co --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/includes/ "$WP_TESTS_DIR/includes"
-svn co --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/data/ "$WP_TESTS_DIR/data"
+download() {
+  if [ "$(command -v curl)" ]; then
+    curl -s "$1" -o "$2"
+  elif [ "$(command -v wget)" ]; then
+    wget -nv -O "$2" "$1"
+  fi
+}
 
-# wp-tests-config.php
-wget -nv -O "$WP_TESTS_DIR/wp-tests-config.php" https://develop.svn.wordpress.org/tags/${WP_VERSION}/wp-tests-config-sample.php
-sed -i "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" "$WP_TESTS_DIR/wp-tests-config.php"
-sed -i "s/youremptytestdbnamehere/$DB_NAME/" "$WP_TESTS_DIR/wp-tests-config.php"
-sed -i "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR/wp-tests-config.php"
-sed -i "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR/wp-tests-config.php"
-sed -i "s|localhost|$DB_HOST|" "$WP_TESTS_DIR/wp-tests-config.php"
+install_wp() {
+  if [ ! -d "$WP_CORE_DIR" ]; then
+    mkdir -p "$WP_CORE_DIR"
+    svn export --quiet https://core.svn.wordpress.org/tags/$WP_VERSION/ "$WP_CORE_DIR"
+  fi
+}
 
-# Download WP Core
-mkdir -p "$WP_CORE_DIR"
-wget -nv -O /tmp/wordpress.tar.gz https://wordpress.org/wordpress-${WP_VERSION}.tar.gz
-tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C "$WP_CORE_DIR"
+install_test_suite() {
+  mkdir -p "$WP_TESTS_DIR"
 
-echo "WordPress and test library installed."
+  svn export --quiet https://develop.svn.wordpress.org/tags/$WP_VERSION/tests/phpunit/includes/ "$WP_TESTS_DIR/includes"
+  svn export --quiet https://develop.svn.wordpress.org/tags/$WP_VERSION/tests/phpunit/data/ "$WP_TESTS_DIR/data"
+  svn export --quiet https://develop.svn.wordpress.org/tags/$WP_VERSION/wp-tests-config-sample.php "$WP_TESTS_DIR/wp-tests-config.php"
+
+  sed -i "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR':" "$WP_TESTS_DIR/wp-tests-config.php"
+  sed -i "s/yourdbnamehere/$DB_NAME/" "$WP_TESTS_DIR/wp-tests-config.php"
+  sed -i "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR/wp-tests-config.php"
+  sed -i "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR/wp-tests-config.php"
+  sed -i "s|localhost|$DB_HOST|" "$WP_TESTS_DIR/wp-tests-config.php"
+}
+
+create_db() {
+  mysqladmin create "$DB_NAME" --user="$DB_USER" --password="$DB_PASS" -h "$DB_HOST" || true
+}
+
+install_wp
+install_test_suite
+create_db
