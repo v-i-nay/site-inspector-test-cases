8459e406e3a46a8c98a49dfa254287c3f847b128
v-i-nay <vinay@softscrips.net>
2025-07-23 17:25:47 +0530

    change

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-# Exit on error
 set -e
 
 DB_NAME=$1
 DB_USER=$2
 DB_PASS=$3
 DB_HOST=$4
-WP_VERSION=$5
+WP_VERSION=${5-latest}
 
-# Fallbacks
-WP_TESTS_DIR=${WP_TESTS_DIR-/tmp/wordpress-tests-lib}
-WP_CORE_DIR=${WP_CORE_DIR-/tmp/wordpress/}
+WP_TESTS_DIR=/tmp/wordpress-tests-lib
+WP_CORE_DIR=/tmp/wordpress
 
 download() {
   if [ "$(which curl)" ]; then
@@ -22,34 +20,28 @@ download() {
 }
 
 install_wp() {
-  if [ -d $WP_CORE_DIR ]; then
-    return
+  if [ ! -d "$WP_CORE_DIR" ]; then
+    mkdir -p "$WP_CORE_DIR"
+    curl -o /tmp/wordpress.tar.gz https://wordpress.org/latest.tar.gz
+    tar -xzf /tmp/wordpress.tar.gz -C /tmp/
   fi
-
-  mkdir -p $WP_CORE_DIR
-  download https://wordpress.org/wordpress-${WP_VERSION}.tar.gz /tmp/wordpress.tar.gz
-  tar --strip-components=1 -zxmf /tmp/wordpress.tar.gz -C $WP_CORE_DIR
 }
 
 install_test_suite() {
-  if [ -d $WP_TESTS_DIR ]; then
-    return
-  fi
+  mkdir -p "$WP_TESTS_DIR"
+  svn export --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/includes/ "$WP_TESTS_DIR/includes"
+  svn export --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/data/ "$WP_TESTS_DIR/data"
+  svn export --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/wp-tests-config-sample.php "$WP_TESTS_DIR/wp-tests-config.php"
 
-  mkdir -p $WP_TESTS_DIR
-  svn co --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/includes/ $WP_TESTS_DIR/includes
-  svn co --quiet https://develop.svn.wordpress.org/tags/${WP_VERSION}/tests/phpunit/data/ $WP_TESTS_DIR/data
-  download https://develop.svn.wordpress.org/tags/${WP_VERSION}/wp-tests-config-sample.php "$WP_TESTS_DIR"/wp-tests-config.php
-
-  sed -i "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR/':" "$WP_TESTS_DIR"/wp-tests-config.php
-  sed -i "s/yourdbnamehere/$DB_NAME/" "$WP_TESTS_DIR"/wp-tests-config.php
-  sed -i "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR"/wp-tests-config.php
-  sed -i "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR"/wp-tests-config.php
-  sed -i "s|localhost|${DB_HOST}|" "$WP_TESTS_DIR"/wp-tests-config.php
+  sed -i "s:dirname( __FILE__ ) . '/src/':'$WP_CORE_DIR/':" "$WP_TESTS_DIR/wp-tests-config.php"
+  sed -i "s/yourdbnamehere/$DB_NAME/" "$WP_TESTS_DIR/wp-tests-config.php"
+  sed -i "s/yourusernamehere/$DB_USER/" "$WP_TESTS_DIR/wp-tests-config.php"
+  sed -i "s/yourpasswordhere/$DB_PASS/" "$WP_TESTS_DIR/wp-tests-config.php"
+  sed -i "s|localhost|$DB_HOST|" "$WP_TESTS_DIR/wp-tests-config.php"
 }
 
 install_db() {
-  mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS" --host="$DB_HOST" || true
+  mysqladmin create "$DB_NAME" --user="$DB_USER" --password="$DB_PASS" -h "$DB_HOST" || true
 }
 
 install_wp
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,12 +1,15 @@
 <?php
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-$_tests_dir = getenv('WP_TESTS_DIR') ?: '/tmp/wordpress-tests-lib';
+if ( ! $_tests_dir ) {
+    $_tests_dir = '/tmp/wordpress-tests-lib';
+}
 
 require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
-    require dirname(__DIR__) . '/wp-site-inspector.php';
+    require dirname( __DIR__ ) . '/wp-site-inspector.php';
 }
-tests_add_filter('muplugins_loaded', '_manually_load_plugin');
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
 require $_tests_dir . '/includes/bootstrap.php';
