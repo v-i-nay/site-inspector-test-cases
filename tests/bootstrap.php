1aebcfda7c26a9cab502301f3629a7a516991700
v-i-nay <vinay@softscrips.net>
2025-07-23 17:34:18 +0530

    change

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,18 @@
 <?php
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
+$_tests_dir = getenv('WP_TESTS_DIR');
 
-if ( ! $_tests_dir ) {
+if (!$_tests_dir) {
     $_tests_dir = '/tmp/wordpress-tests-lib';
 }
 
+// Load WordPress test functions
 require_once $_tests_dir . '/includes/functions.php';
 
-function _manually_load_plugin() {
-    require dirname( __DIR__ ) . '/wp-site-inspector.php';
+// Manually load plugin
+function _load_plugin() {
+    require dirname(__DIR__) . '/wp-site-inspector.php'; // Adjust to your plugin’s main file
 }
-tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+tests_add_filter('muplugins_loaded', '_load_plugin');
 
+// Start up the WP testing environment
 require $_tests_dir . '/includes/bootstrap.php';
